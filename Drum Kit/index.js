3708239c1b817ffa532ac769c6c7eca89214fbd2
Kevin Pham <phambamkevin@gmail.com>
2020-08-07 10:27:19 -0700

    handleClick vs handleClick()

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,4 +1,4 @@
-document.querySelector("button").addEventListener("click", handleClick);
+document.querySelector("button").addEventListener("click", handleClick); // Notice that its not "handleClick()" as this is an instant function call
 
 function handleClick() {
   alert("I got clicked!");
