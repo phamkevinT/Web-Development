d8ca51e5eba5e98b2ef1e7f64e44c1ffdeb724a3
Kevin Pham <phambamkevin@gmail.com>
2020-08-14 13:12:59 -0700

    Handling Request and Responses: GET Request

--- ExpressNodeJs/server.js
+++ ExpressNodeJs/server.js
@@ -4,6 +4,11 @@ const express = require("express");
 
 const app = express();
 
+// What should happen when browser gets in touch with server and makes GET request
+app.get("/", function(request, response) {
+  response.send("<h1>Hello, world!</h1>");
+});
+
 // Listen to HTTP request on server
 app.listen(3000, function() {
   console.log("Server started on port 3000");
