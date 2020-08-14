3211d54c7a5e1ed07925e16eec02d07b100af44c
Kevin Pham <phambamkevin@gmail.com>
2020-08-14 13:42:51 -0700

    Practice

--- ExpressNodeJs/server.js
+++ ExpressNodeJs/server.js
@@ -4,9 +4,19 @@ const express = require("express");
 
 const app = express();
 
-// What should happen when browser gets in touch with server and makes GET request
+// What should happen when browser gets in touch with server and makes GET request at Home Page
 app.get("/", function(request, response) {
   response.send("<h1>Hello, world!</h1>");
+});
+
+// Contact Page GET Request
+app.get("/contact", function(req, res) {
+  res.send("Contact me at: phambamkevin@gmail.com");
+});
+
+// About Page GET Request
+app.get("/about", function(req, res) {
+  res.send("This is a bio page about me.");
 });
 
 // Listen to HTTP request on server
