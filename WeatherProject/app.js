a06d7335a77ba12041662ec50f0f291d00caf457
Kevin Pham <phambamkevin@gmail.com>
2020-08-18 15:54:33 -0700

    Parsing JSON

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -7,7 +7,26 @@ app.get("/", function(req, res) {
 
   const url = "https://api.openweathermap.org/data/2.5/weather?q=San Jose&appid=a956a4d31c0ebed91fd0619bdbd3c6c7&units=imperial";
   https.get(url, function(response) {
-    console.log(response);
+    console.log(response.statusCode);
+
+
+    response.on("data", function(data) {
+      const weatherData = JSON.parse(data);
+
+      const temp = weatherData.main.temp;
+      console.log(temp);
+
+      const description = weatherData.weather[0].description;
+      console.log(description);
+
+      // Turning Object into a String
+          // const object = {
+          //   name: "Kevin",
+          //   favFood: "Ramen"
+          // }
+          // console.log(JSON.stringify(object));
+
+    })
   })
 
 
