1c2b7aab56cd30dcf2cd2ed10a87df18b45d5ba3
Kevin Pham <phambamkevin@gmail.com>
2020-08-19 15:12:26 -0700

    Subscriberlist works with MailChimp

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -16,11 +17,40 @@ app.get("/", function(req, res) {
 
 
 app.post("/", function(req, res) {
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  console.log(firstName, lastName, email);
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  // Turn the data/info above to a JSON object
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us17.api.mailchimp.com/3.0/lists/ad76f36bac";
+  const options = {
+      method: "POST",
+      auth: "kevin1:7f3725f2e5a1236b45ab576f6f78b07c-us17"
+  }
+
+  const request = https.request(url, options, function(response) {
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 
 });
 
