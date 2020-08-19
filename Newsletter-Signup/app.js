1776347a6edd661ca23137ba24be7fa97b133c18
Kevin Pham <phambamkevin@gmail.com>
2020-08-19 16:03:38 -0700

    adjusted ports to work on both heroku and port 3000

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -67,6 +67,6 @@ app.post("/failure", function(req, res) {
 });
 
 // Running the server on localhost port 3000
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running on port 3000.")
 });
