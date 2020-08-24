568124c668898e108530d7c5d5d1ca1ae9741135
Kevin Pham <phambamkevin@gmail.com>
2020-08-24 14:01:37 -0700

    Passing Data to Server

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,51 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set('view engine', 'ejs'); // Tells how app to use EJS
-
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function (req, res) {
 
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thurday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: Current day is equal to " + currentDay);
-  }
+  var day = today.toLocaleDateString("en-US", options);
+
 
   // Render a file called "list" inside the "views" folder with extenstion ".ejs". "kindOfDay" is a KEY in the list.ejs
-  res.render("list", {
-    kindOfDay: day
-  });
+  res.render("list", {kindOfDay: day, newListItems: items});
 
 });
 
 // This is another test commit
 // This is also another test commit
+
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem; 
+  
+  items.push(item);
+
+  // When a post a request is triggered, save newItem to item and redirect to the home route which goes to the app.get() above.
+  res.redirect("/"); 
+
+});
 
 
 app.listen(3000, function () {
